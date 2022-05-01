06bdd9fc6fddb6541f583eea13419ffde5ce0b77
riisatoro <riisa.toro@gmail.com>
2022-05-01 20:15:23 +0300

    Add password hash and compare functions

---
+++ backend/operations/hashers.py
@@ -0,0 +1,28 @@
+import hashlib
+import hmac
+import os
+
+from typing import Tuple
+
+
+def hash_password(password: str) -> Tuple[bytes, bytes]:
+    """
+    Hash the provided password with a pepper salt and return the
+    hash to store in the database.
+    """
+    pepper = os.getenv('SECRET_KEY')
+    if not pepper:
+        raise ValueError('SECRET_KEY is required')
+    pw_hash = hashlib.pbkdf2_hmac('sha256', password.encode(), pepper.encode(), 100000)
+    return pw_hash
+
+
+def is_passwords_match(pw_hash: bytes, password: str) -> bool:
+    """
+    Check whether the password is correct.
+    """
+    pepper = os.getenv('SECRET_KEY')
+    return hmac.compare_digest(
+        pw_hash,
+        hashlib.pbkdf2_hmac('sha256', password.encode(), pepper.encode(), 100000)
+    )
