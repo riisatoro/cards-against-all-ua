56fbf415f6a55ee068a0408a42f59a810fd597e8
riisatoro <riisa.toro@gmail.com>
2022-05-18 17:11:42 +0300

    Create models for cards and user game rooms

---
+++ backend/database/enums.py
@@ -0,0 +1,6 @@
+from enum import Enum
+
+
+class CardsEnum(Enum):
+    question = 'question'
+    answer = 'answer'
--- backend/database/models.py
+++ backend/database/models.py
@@ -1,12 +1,18 @@
 from datetime import datetime
+from enums import CardsEnum
 from uuid import uuid4
 
 from sqlalchemy import (
+    Boolean,
     Column,
     DateTime,
+    Integer,
     ForeignKey,
     LargeBinary,
     String,
+    Text,
+    Enum,
+    UniqueConstraint
 )
 from sqlalchemy.ext.declarative import declared_attr, declarative_base
 from sqlalchemy.orm import relationship
@@ -38,3 +44,41 @@ class UserTokenModel(Model):
     refresh = Column(String, nullable=False)
 
     user = relationship('UserModel', back_populates='refresh')
+
+
+class CardsModel(Model):
+    text = Column(Text, nullable=False)
+    card_type = Column(Enum(CardsEnum), nullable=False)
+
+
+class RoomModel(Model):
+    is_started = Column(Boolean, default=False)
+    is_ended = Column(Boolean, default=False)
+
+    round_number = Column(Integer, default=0)
+    round_end_time = Column(DateTime, nullable=False)
+
+    leader = Column(String, ForeignKey('usermodel.id'), nullable=True)
+    question_card = Column(String, ForeignKey('cardsmodel.id'), nullable=True)
+    funny_card = Column(String, ForeignKey('cardsmodel.id'), nullable=True)
+
+
+class UserRoomModel(Model):
+    __table_args__ = (
+        UniqueConstraint('user_id', 'room_id'),
+    )
+
+    user_id = Column(String, ForeignKey('usermodel.id'), nullable=True)
+    room_id = Column(String, ForeignKey('roommodel.id'), nullable=True)
+
+    user_score = Column(Integer, default=0)
+    answer_card = Column(String, ForeignKey('cardsmodel.id'))
+
+
+class UserRoomCardsModel(Model):
+    __table_args__ = (
+        UniqueConstraint('user_room_id', 'card_id'),
+    )
+
+    user_room_id = Column(String, ForeignKey('userroommodel.id'), nullable=False)
+    card_id = Column(String, ForeignKey('cardsmodel.id'), nullable=False)
