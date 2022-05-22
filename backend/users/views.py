9e9343c27dad383d7cbb1863f220944e0c688a58
riisatoro <riisa.toro@gmail.com>
2022-05-22 17:35:47 +0300

    Add tags for documentation

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.hashers import make_password
+from drf_spectacular.utils import extend_schema
 from rest_framework.generics import CreateAPIView
 
-from authentication.models import User
+
 from users.serializers import CreateUserSerializer, SaveUserSerializer
 
 
+@extend_schema(tags=('User',))
 class CreateUserView(CreateAPIView):
     serializer_class = CreateUserSerializer
     authentication_classes = []
