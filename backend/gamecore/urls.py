fd920a4600289c6238599ef6f3bb0d29aced5951
riisatoro <riisa.toro@gmail.com>
2022-06-07 19:11:35 +0300

    Add backslash for urlpatterns

--- backend/gamecore/urls.py
+++ backend/gamecore/urls.py
@@ -4,7 +4,7 @@ from gamecore.views import UserCreateRoomView, UserJoinRoomView, UserLeaveRoomVi
 
 
 urlpatterns = [
-    path('create', UserCreateRoomView.as_view(), name='create_game'),
+    path('create/', UserCreateRoomView.as_view(), name='create_game'),
     path('join/', UserJoinRoomView.as_view(), name='join_random_game'),
     path('join/<uuid:room_uuid>/', UserJoinRoomView.as_view(), name='join_concrete_game'),
     path('leave/', UserLeaveRoomView.as_view(), name='leave_game'),
