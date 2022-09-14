8a1a4cd3351d979d6eba59b1bb418267e44f71c1
riisatoro <riisa.toro@gmail.com>
2022-09-14 23:16:55 +0300

    !IMPORTANT! Temporary remove socket authorization

--- backend/gamesocket/consumers.py
+++ backend/gamesocket/consumers.py
@@ -5,14 +5,9 @@ from asgiref.sync import async_to_sync
 
 class GameRoomNotifications(WebsocketConsumer):
     def connect(self):
-        user = self.scope['user']
         self.room_id = self.scope["url_route"]["kwargs"]["room_id"]
-
-        if user.is_authenticated:
-            self.accept()
-            async_to_sync(self.channel_layer.group_add)(self.room_id, self.channel_name)
-        else:
-            self.close()
+        self.accept()
+        async_to_sync(self.channel_layer.group_add)(self.room_id, self.channel_name)
 
     def disconnect(self, close_code):
         self.channel_layer.group_discard(self.room_id, self.channel_name)
